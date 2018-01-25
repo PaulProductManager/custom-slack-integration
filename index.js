a1753fc9c2a846f1a456bdf4e61af46d5b0d2781
Michelle Savage <michelle@promoboxx.com>
2018-01-25 16:52:01 -0500

    updated

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ const	express = 	require('express'),
 app.use(bodyParser.json());
 
 app.post('/', function(req, res){
-	if (req.get('X-GitHub-Event') == 'watch'){
-		slack.sendMessage({
-			'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> ' + req.body.action + ' watching <' + req.body.repository.url + '|' + req.body.repository.name + '>'
-		});
-	}
-	if (req.get('X-GitHub-Event') == 'fork'){
-		slack.sendMessage({
-			'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> forked <' + req.body.repository.url + '|' + req.body.repository.name + '>',
-		});
-	}
+	slack.sendMessage({
+		'text': 'testing'
+	});
+	// if (req.get('X-GitHub-Event') == 'watch'){
+	// 	slack.sendMessage({
+	// 		'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> ' + req.body.action + ' watching <' + req.body.repository.url + '|' + req.body.repository.name + '>'
+	// 	});
+	// }
+	// if (req.get('X-GitHub-Event') == 'fork'){
+	// 	slack.sendMessage({
+	// 		'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> forked <' + req.body.repository.url + '|' + req.body.repository.name + '>',
+	// 	});
+	// }
 	res.sendStatus(200);
 });
 
