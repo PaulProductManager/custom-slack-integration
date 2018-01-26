49bd9aaf9aceec4115d25180432347eff3d6b318
Michelle Savage <michelle@promoboxx.com>
2018-01-26 18:38:11 -0500

    updated

--- index.js
+++ index.js
@@ -3,25 +3,46 @@ const	express = 	require('express'),
 	bodyParser = 	require('body-parser'),
 	port = 		process.env.PORT || 8080,
 	slack = 	require('./slack.js');
+const content_title, content_msg, content_button_msg, content_button_url, content_button_fallback;
 
 app.use(bodyParser.json());
 
 app.post('/', function(req, res){
-	if (req.get('X-GitHub-Event') == 'watch'){
-		slack.sendMessage({
-			'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> ' + req.body.action + ' watching <' + req.body.repository.url + '|' + req.body.repository.name + '>'
-		});
+	// Create content, depending on incoming source: GitHub, Jira
+	if (req.get('X-GitHub-Event')) {
+		switch (req.get('X-GitHub-Event')) {
+			case 'push':
+			case 'fork':
+			case 'watch':
+				content_title = '*<' + req.body.sender.url + '|' + req.body.sender.login + '>* requests your Code Review for Pull Request <' + req.body.repository.url + '|' + req.body.repository.name + '>';
+				content_button_msg = 'View Pull Request';
+				break;
+		}
 	}
-	if (req.get('X-GitHub-Event') == 'push'){
-		slack.sendMessage({
-			'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> pushed to <' + req.body.repository.url + '|' + req.body.repository.name + '>',
-		});
-	}
-	// if (req.get('X-GitHub-Event') == 'fork'){
-	// 	slack.sendMessage({
-	// 		'text': '<' + req.body.sender.url + '|' + req.body.sender.login + '> forked <' + req.body.repository.url + '|' + req.body.repository.name + '>',
-	// 	});
-	// }
+	// Send message
+	slack.sendMessage({
+		'text': content_title,
+		'attachments': [
+			{
+				'text': '',
+				'fallback': 'You are unable to choose an action',
+				'callback_id': 'wopr_game',
+				'color': '#ffffff',
+				'attachment_type': 'default',
+				'actions': [
+					{
+						'name': 'github',
+						'text': content_button_msg,
+						'style': 'primary',
+						'type': 'button',
+						'value': 'pull_request'
+					}
+				]
+			}
+		]
+
+
+	});
 	res.sendStatus(200);
 });
 
