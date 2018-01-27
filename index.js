e46b07d2ca68bd7855a063da74d5fcc0e820cab0
Michelle Savage <michelle@promoboxx.com>
2018-01-26 19:09:03 -0500

    updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/', function(req, res){
 			case 'push':
 			case 'fork':
 			case 'watch':
-				content_title = 'test'; //'*<' + req.body.sender.url + '|' + req.body.sender.login + '>* requests your Code Review for Pull Request <' + req.body.repository.url + '|' + req.body.repository.name + '>';
+				content_title = 'test';
 				content_button_msg = 'View Pull Request';
 				break;
 		}
@@ -27,6 +27,9 @@ app.post('/', function(req, res){
 	slack.sendMessage({
 		'text': content_title
 	});
+
+ //'*<' + req.body.sender.url + '|' + req.body.sender.login + '>* requests your Code Review for Pull Request <' + req.body.repository.url + '|' + req.body.repository.name + '>';
+
 		// 'attachments': [
 		// 	{
 		// 		'text': '',
