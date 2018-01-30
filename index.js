97398bb3412f8ce70231fd576b18211762ffa330
Michelle Savage <michelle@promoboxx.com>
2018-01-30 17:34:37 -0500

    updated

--- index.js
+++ index.js
@@ -123,6 +123,4 @@ Tasks:
 
 - Create user-friendly UI where anyone can "link" up new users with their Slack-Channel-ID and Jira-ID (and Github-ID)
 	- see: https://api.slack.com/methods/channels.list
-
-
 */
