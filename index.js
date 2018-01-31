111ab3bc0e9b1d98cafa3cb959df3c0f2af19620
Michelle Savage <michelle@promoboxx.com>
2018-01-31 16:59:38 -0500

    updated

--- index.js
+++ index.js
@@ -68,7 +68,26 @@ app.post('/', function(req, res){
 	}
 
 	if (in_header_user_agent.indexOf("atlassian") > -1) {
-		out_title = 'out: ' + req;
+		var flattenObject = function(req) {
+			var toReturn = {};
+
+			for (var i in ob) {
+				if (!ob.hasOwnProperty(i)) continue;
+
+				if ((typeof ob[i]) == 'object' && ob[i] !== null) {
+					var flatObject = flattenObject(ob[i]);
+					for (var x in flatObject) {
+						if (!flatObject.hasOwnProperty(x)) continue;
+
+						toReturn[i + '.' + x] = flatObject[x];
+					}
+				} else {
+					toReturn[i] = ob[i];
+				}
+			}
+			return toReturn;
+		};
+		out_title = 'out: ' + flattenObject;
 		out_channel.push('@U9159L4KE');
 	}
 
