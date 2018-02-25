ecf8ccb7543654636e3b4fb00fbc4bbefa655860
Michelle Savage <michelle@promoboxx.com>
2018-02-24 19:56:14 -0500

    updated

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/', function(req, res){
       case 'pull_request_review_comment':
       case 'issue_comment':
         // Create msg
-        out_title = '*' + req.body.sender.login + '* mentioned you in PR #<' + req.body.issue.pull_request.html_url + '|' + req.body.issue.number + '>';
+        out_title = '*' + req.body.sender.login + '* mentioned you in a Comment for PR #<' + req.body.issue.pull_request.html_url + '|' + req.body.issue.number + '>';
 
         // Look for Github Mentions within the Comment-Body
         out_channel_blob = out_channel_blob + req.body.comment.body;
@@ -98,7 +98,7 @@ app.post('/', function(req, res){
         // Create msg
         out_title = '*' + req.body.sender.login + '* mentioned you in PR #<' + req.body.pull_request.html_url + '|' + req.body.pull_request.number + '>';
 
-        out_title = out_title + ' *** ' + out_channel_blob;
+        // out_title = out_title + ' *** ' + out_channel_blob;
 
         // Create list of users: get github Requested Reviewers
         for (var r = 0; r < req.body.pull_request.requested_reviewers.length; r++) {
