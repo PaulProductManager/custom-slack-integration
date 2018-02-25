34a4041eda52f9daaa979d8e6276aef81dfb20d1
Michelle Savage <michelle@promoboxx.com>
2018-02-24 21:30:19 -0500

    updated

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post('/', function(req, res){
 	        out_title = '*' + req.body.sender.login + '* mentioned you in PR #<' + req.body.pull_request.html_url + '|' + req.body.pull_request.number + '>';
 
 	        // Look for Github Mentions within the Description-Body
-	        out_channel_blob = req.body.comment.body;
+	        out_channel_blob = req.body.pull_request.body;
 	        out_channel = out_channel_blob.match(github_mention_regex);
 
 	        // Create list of users: get github Requested Reviewers
