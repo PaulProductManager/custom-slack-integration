379caf04c257f7288b5702fd3b06f16e6309d8ab
Michelle Savage <michelle@promoboxx.com>
2018-02-24 20:11:22 -0500

    updated

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/', function(req, res){
         // out_channel = out_channel_blob.match(github_mention_regex);
 
         out_title = out_title + ' *** ' + out_channel_blob;		// TEST
-        out_channel.push('@hujambo-dunia');
+        out_channel.push('hujambo-dunia');
 
         break;
       case 'pull_request_review':
